3560c0a4de5e2b7fb873d5b88f5e6aa34f40a571
AlanTeixido <alan.teixido.7e7@itb.cat>
2024-11-26 19:54:28 +0100

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+import data from '@/assets/data.json'; 
+
+export default {
+  mounted() {
+    console.log('Datos cargados del JSON:', data);
+  },
+};
+</script>
